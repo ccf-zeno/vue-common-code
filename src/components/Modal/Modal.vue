b32158ed601059c115d6556ae8edbbecd3d4b209
ccf-zeno <zhongzt177@gmail.com>
2025-04-22 22:45:59 +0800

    feat: 新增modal

---
+++ src/components/Modal/Modal.vue
@@ -0,0 +1,75 @@
+<script lang="ts">
+import type { PropType } from "vue";
+import { computed, defineComponent } from "vue";
+import { Modal } from "@arco-design/web-vue";
+import { omit } from "lodash-es";
+
+import { handleEmits } from "@/utils";
+
+export default defineComponent({
+  name: "MiModal",
+  components: {
+    ArcoModal: Modal,
+  },
+  extends: Modal,
+  inheritAttrs: false,
+  props: {
+    onBeforeOk: {
+      type: Function as PropType<
+        () => void | boolean | Promise<void | boolean>
+      >,
+    },
+    width: {
+      type: [Number, String],
+      default: 500,
+    },
+  },
+  setup: (props, { emit, slots }) => {
+    const aProps = computed(() => {
+      return omit(props, ["onBeforeOk", "width"]) as any;
+    });
+    const aSlots = slots as any;
+    const aEmits = handleEmits(Modal.emits, emit);
+
+    async function onBeforeOk() {
+      try {
+        return await props.onBeforeOk?.();
+      } catch {
+        return false;
+      }
+    }
+
+    return {
+      aProps,
+      aSlots,
+      aEmits,
+      onBeforeOk,
+    };
+  },
+});
+</script>
+
+<template>
+  <ArcoModal
+    v-bind="{ ...aProps, ...$attrs }"
+    :onBeforeOk="onBeforeOk"
+    :style="{
+      '--mi-modal-width':
+        typeof $props.width === 'number' ? `${$props.width}px` : $props.width,
+    }"
+    :class="['mi-modal', $attrs.class]"
+    v-on="aEmits"
+  >
+    <template v-for="(item, key) in aSlots" #[key]>
+      <slot :name="key"></slot>
+    </template>
+  </ArcoModal>
+</template>
+
+<style lang="less">
+.mi-modal {
+  .arco-modal {
+    width: var(--mi-modal-width);
+  }
+}
+</style>
---
+++ src/components/Modal/index.ts
@@ -0,0 +1,35 @@
+import type { AppContext } from 'vue';
+import { Modal as ArcoModal } from '@arco-design/web-vue';
+import type { ModalConfig } from '@arco-design/web-vue';
+
+import MiModal from './Modal.vue';
+
+// 包装器函数
+const createModalMethod = (method: keyof typeof ArcoModal) => {
+  return (config: ModalConfig, appContext?: AppContext) => {
+    return ArcoModal[method](
+      {
+        ...config,
+        onBeforeOk: async (done: (v: boolean) => void) => {
+          try {
+            return (await config.onBeforeOk?.(done)) ?? true;
+          } catch {
+            return false;
+          }
+        },
+      },
+      appContext,
+    );
+  };
+};
+
+export const Modal = Object.assign(MiModal, {
+  install: ArcoModal.install,
+  _context: ArcoModal._context,
+  open: createModalMethod('open'),
+  confirm: createModalMethod('confirm'),
+  info: createModalMethod('info'),
+  success: createModalMethod('success'),
+  warning: createModalMethod('warning'),
+  error: createModalMethod('error'),
+});
